5bbde9e2da41aed3b87790d8db6c3c226858e9ec
Shiho Nagano <shiho@nagano.com>
2021-12-13 18:37:31 -0800

    fixed js

--- main.js
+++ main.js
@@ -59,15 +59,15 @@ const contactY = contactId.getBoundingClientRect().top;
 window.addEventListener('scroll', () => {
   const windowTopDistance = window.pageYOffset;
 
-    if (windowTopDistance > menuY && aboutY > windowTopDistance){
+    if (windowTopDistance >= menuY && aboutY > windowTopDistance){
       menuLink.classList.add('turn-black');
       aboutLink.classList.remove('turn-black');
       contactLink.classList.remove('turn-black');
-    }else if(windowTopDistance > aboutY && contactY > windowTopDistance){
+    }else if(windowTopDistance >= aboutY && contactY > windowTopDistance){
       aboutLink.classList.add('turn-black');
       menuLink.classList.remove('turn-black');
       contactLink.classList.remove('turn-black');
-    }else if(windowTopDistance > contactY){
+    }else if(windowTopDistance >= contactY){
       contactLink.classList.add('turn-black');
       aboutLink.classList.remove('turn-black');
       menuLink.classList.remove('turn-black')
